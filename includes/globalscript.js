5faebc09426ba35a1641204f0ab0bb9a60d17290
Moeshh <mex_ncn@hotmail.com>
2023-03-22 18:52:42 +0100

    register en login fix

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -1,25 +1,13 @@
 const BASEURL = "https://localhost:7199";
 
 window.onload = function(){
-    var arr = JSON.parse( localStorage.getItem('userData') );
+
+    //localStorage.clear();
+    /*var arr = JSON.parse( localStorage.getItem('userData') );
     console.log(arr["Usertype"]);
 
     document.getElementById("usertype-menu-item").innerHTML = arr["Usertype"];
-    //localStorage.clear();
-}
-
-function submitForm(e) {
-    e.preventDefault();
-
-    fetch('https://localhost:7199/api/Users/check', {
-    
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        updateUserTypeMenuItem(data.usertype);
-    })
-    .catch(error => console.error(error));
+    */
 }
 
 function updateUserTypeMenuItem(usertype) {
