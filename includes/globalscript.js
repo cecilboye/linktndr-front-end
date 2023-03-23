dc129f0ce3a81832532793cad1fd9476a15c44d5
Martijn Hiemstra <martyn.hiemstra@gmail.com>
2023-03-23 16:07:58 +0100

    Added logged in user test

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -1,14 +1,24 @@
 const BASEURL = "https://localhost:7199";
 
-window.onload = function(){
-
-    //localStorage.clear();
-    if (localStorage.getItem("UserData") === null) {
+function onPageLoad(after) {
+    if (!localStorage.getItem("Token")) {
         window.location.href = "./";
     }else{
-        var userData = JSON.parse(localStorage.getItem('UserData') );
-        console.log(userData);
-        document.getElementById("usertype-menu-item").innerHTML = userData["usertype"];
+        var userToken = localStorage.getItem('Token');
+        if (!!userToken) {
+            fetch('https://localhost:7199/api/user', {
+                headers: {
+                    'Authorization': userToken
+                }
+            })
+            .then(response => response.json())
+            .then(userData => {
+                document.getElementById("usertype-menu-item").innerHTML = userData.name;
+
+                after(userData);
+            });
+        }
+
     }    
 }
 
@@ -16,3 +26,4 @@ function updateUserTypeMenuItem(usertype) {
     const usertypeMenuItem = document.getElementById('usertype-menu-item');
     usertypeMenuItem.innerHTML = `<a href="#">${usertype}</a>`;
 }
+
