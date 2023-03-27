4b8604e215cdda7e9fdb53b9435c09a8e07f4910
DavidVanMeel <davidvanmeel@hotmail.nl>
2023-03-27 13:01:39 +0200

    1

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -2,9 +2,15 @@ const BASEURL = "https://localhost:7199";
 
 // after is een anonymous function
 function onPageLoad(after) {
-    if (!localStorage.getItem("Token")) {
-        window.location.href = "./index.html";
-    }else{
+    if (!localStorage.getItem("Token")) 
+    {
+        if (window.location != "./index.html")
+        {
+            window.location.href = "./index.html";
+        }
+    }
+    else
+    {
         var userToken = localStorage.getItem('Token');
         if (!!userToken) {
             fetch('https://localhost:7199/api/user', {
@@ -19,8 +25,12 @@ function onPageLoad(after) {
                 after(userData);
             });
         }
-
     }    
+}
+           
+function logout(){
+    localStorage.clear();
+    window.location.href = "./";
 }
 
 function updateUserTypeMenuItem(usertype) {
