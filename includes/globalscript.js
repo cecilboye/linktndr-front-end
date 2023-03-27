61dee6247b7c8a8489ac031ae7955fef4085ad72
Martijn Hiemstra <martyn.hiemstra@gmail.com>
2023-03-27 13:13:00 +0200

    Added header.html include

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -1,16 +1,41 @@
 const BASEURL = "https://localhost:7199";
 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("lt-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("lt-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
+
 // after is een anonymous function
 function onPageLoad(after) {
-    if (!localStorage.getItem("Token")) 
-    {
-        if (window.location != "./index.html")
-        {
+    if (!localStorage.getItem("Token")) {
+        if (window.location != "./index.html") {
             window.location.href = "./index.html";
         }
     }
-    else
-    {
+    else {
         var userToken = localStorage.getItem('Token');
         if (!!userToken) {
             fetch('https://localhost:7199/api/user', {
@@ -18,17 +43,19 @@ function onPageLoad(after) {
                     'Authorization': userToken
                 }
             })
-            .then(response => response.json())
-            .then(userData => {
-                document.getElementById("usertype-menu-item").innerHTML = userData.name;
+                .then(response => response.json())
+                .then(userData => {
+                    includeHTML();
 
-                after(userData);
-            });
+                    // document.getElementById("usertype-menu-item").innerHTML = userData.name;
+
+                    after(userData);
+                });
         }
-    }    
+    }
 }
-           
-function logout(){
+
+function logout() {
     localStorage.clear();
     window.location.href = "./";
 }
