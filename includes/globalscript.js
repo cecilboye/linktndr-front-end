e13df7e1c5ed3df3cfb4533e61b7b6a9b33b8284
Moeshh <mex_ncn@hotmail.com>
2023-03-28 14:56:28 +0200

    fixed modals and started on talentmanager page

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -14,8 +14,8 @@ function onPageLoad(after) {
             })
             .then(response => response.json())
             .then(userData => {
+                console.log(userData);
                 document.getElementById("usertype-menu-item").innerHTML = userData.name;
-
                 after(userData);
             });
         }
@@ -26,6 +26,17 @@ function onPageLoad(after) {
 function updateUserTypeMenuItem(usertype) {
     const usertypeMenuItem = document.getElementById('usertype-menu-item');
     usertypeMenuItem.innerHTML = `<a href="#">${usertype}</a>`;
+}
+
+function formattedDate(d = new Date) {
+    let month = String(d.getMonth() + 1);
+    let day = String(d.getDate());
+    const year = String(d.getFullYear());
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return `${day}/${month}/${year}`;
 }
 
 function logout(){
