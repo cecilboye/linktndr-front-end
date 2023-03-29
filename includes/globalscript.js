5ad8a35026d1af13f8173fa05eda5922f511e4c5
Moeshh <mex_ncn@hotmail.com>
2023-03-29 10:08:27 +0200

    bugfix

--- includes/globalscript.js
+++ includes/globalscript.js
@@ -1,5 +1,63 @@
 const BASEURL = "https://localhost:7199";
 
+function onPageLoad(after) {
+    if (!localStorage.getItem("Token")) {
+        let path = window.location.pathname.split("/").pop();
+        if (path == "register.html") {
+            AddHeader();
+        } else {
+            window.location.href = "./index.html";
+        }
+    }
+    else {
+        var userToken = localStorage.getItem('Token');
+        if (!!userToken) {
+            fetch(URL + 'user', {
+                headers: {
+                    'Authorization': userToken
+                }
+            })
+                .then(response => response.json())
+                .then(userData => {
+                    AddHeader(userData);
+                    after(userData);
+                });
+        }
+    }
+}
+
+function AddHeader(data = null) {
+    console.log(data);
+    let name = (data!='Login' || data!='Register')? data:data.name;
+    let logoutButton = (data!='Login' || data!='Register')? '':'<button onclick="logout()" class="btn btn-secondary" style="color: white; border-color: white; background-color: transparent; margin-right: 10px;">Log Out</button>';
+    let href = (data=='register')? './register.html':'./';
+
+    document.getElementById("header").innerHTML=
+    `<nav class="navbar navbar-inverse" style="border-color: rgb(254, 75, 16); background-image: linear-gradient(rgb(253, 43, 122),rgb(255, 106, 91));">
+        <div class="navbar-header">
+            <a href="index.html">
+                <img src="img/logo_header.png" style="height: 40px; width: 40px; margin-top: 5px; margin-bottom: 5px; margin-left: 15px;"></img>
+            </a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <div>
+                <div id="logout" class="item-ingelogd" style="color:white; margin-right: 15px;">${logoutButton}
+                    <a id="userName" href="${href}" style="color: white;">
+                        <span class="glyphicon glyphicon-user" style="color: white;"></span>
+                        ${name}
+                    </a>
+                </div>
+            </div>
+        </ul>
+    </nav>`;
+
+    document.getElementById("footer").innerHTML=
+    `<div>
+        <div class="container-fluid" style="background-image: linear-gradient(white, rgb(255, 163, 155)); position: fixed; bottom: 3%; width: 100%; height: 8%; z-index: -1;"></div>
+        <div class="container-fluid" style="background-image: linear-gradient(rgb(255, 163, 155), rgb(255, 106, 91)); position: fixed; bottom: 0%; width: 100%; height: 3%; z-index: -1; color: white; text-align: end; font-size: x-small;">LinkTindr® is een product van KlasVanFebruari LLC inc.</div>
+    </div>`;
+}
+/*
 // after is een anonymous function
 function onPageLoad(after) {
     if (!localStorage.getItem("Token")) {
@@ -88,7 +146,7 @@ function AddHeader(data) {
         <div class="container-fluid" style="background-image: linear-gradient(rgb(255, 163, 155), rgb(255, 106, 91)); position: fixed; bottom: 0%; width: 100%; height: 3%; z-index: -1; color: white; text-align: end; font-size: x-small;">LinkTindr® is een product van KlasVanFebruari LLC inc.</div>
     </div>`;
 }
-
+*/
 function logout() {
     localStorage.clear();
     window.location.href = "./";
